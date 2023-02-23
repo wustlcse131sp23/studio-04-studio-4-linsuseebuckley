d3f036617dc06a3c40d05b765fae43655493b352
zachsusee <zachsusee@localhost>
2023-02-23 15:36:21 -0600

    bruh

--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -29,32 +29,44 @@ public class NorwayMotherOfAllFlags {
 	}
 
 	public static void france() {
-		
+		StdDraw.setXscale(5, 8);
+		StdDraw.setYscale(0, 4);
+		mother();
 		// TODO
 	}
 
 	public static void indonesia() {
-		
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(9, 11);
+		mother();
 		// TODO
 	}
 
 	public static void netherlands() {
-		
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(8, 5);
+		mother();
 		// TODO
 	}
 
 	public static void poland() {
-		
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(5, 7);
+		mother();
 		// TODO
 	}
 
 	public static void thailand() {
-		
+		StdDraw.setXscale(0, 6);
+		StdDraw.setYscale(11, 5);
+		mother();
 		// TODO
 	}
 
 	public static void finland() {
-		
+		StdDraw.setXscale(6, 13);
+		StdDraw.setYscale(10, 6);
+		mother();
 		// TODO
 	}
 
